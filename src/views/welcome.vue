3389069afa8196245842fd341c6be5119cb19626
eric-chen <eric-pw@qq.com>
2019-08-28 13:25:25 +0800

    首页

---
+++ src/views/welcome.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="wel">
+        <h1>欢迎登陆电商后台管理系统</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
